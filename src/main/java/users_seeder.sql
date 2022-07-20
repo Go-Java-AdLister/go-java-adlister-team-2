c01bd945e4e0a9970485365a06ea253062e56bb7
Chase Martinez <chasemartinez22@gmail.com>
2022-07-20 17:00:51 -0500

    Updating seeder files

--- src/main/java/users_seeder.sql
+++ src/main/java/users_seeder.sql
@@ -1,7 +1,7 @@
 USE adlister_db;
 
-INSERT INTO users (id, username, email, password)
-VALUES (('dracula', 'thecount@transylvalnia.com', 'blood123'),
+INSERT INTO users (username, email, password)
+VALUES ('dracula', 'thecount@transylvalnia.com', 'blood123'),
         ('bob', 'bob@bob.net', 'bobpass'),
         ('chrislee', 'beaker@muppets.org', 'spongebob'),
-        ('spicynoodles', 'ramenenjoyer@yahoo.com', 'legitbussin'));
+        ('spicynoodles', 'ramenenjoyer@yahoo.com', 'legitbussin');
